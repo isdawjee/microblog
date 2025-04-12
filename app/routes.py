fa506b23b3a47ea45f0d411374a07d5e843e7fb4
Ismail S. Dawjee <ismail.dawjee@gmail.com>
2025-04-13 01:54:43 +0200

    Loops, conditional statements and template inheritance; setup new home page with base that is extended on index.html

--- app/routes.py
+++ app/routes.py
@@ -7,4 +7,14 @@ from app import app
 @app.route("/index")
 def index():
     user = {"username": "Ismail"}
-    return render_template("index.html", title="Home", user=user)
+    posts = [
+        {
+            "author": {"username": "Sadiyah"},
+            "body": "Beautiful day for some MongoDB queries! 👩‍💻",
+        },
+        {
+            "author": {"username": "Tasmiyah"},
+            "body": "My Father made me card story to prioritize leaning flask 🤭",
+        },
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
