f55a9510cf208950aeb8a3d7828251648a3f6a12
Ismail S. Dawjee <ismail.dawjee@gmail.com>
2025-04-13 02:40:41 +0200

    Built login form with views, included redirects with url_for. Setup config object for csrf and forms submissions. Created user login form with validators and template file, which also includes flash messages

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from config import Config
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 # Must be here, otherwise circular import issue
 from app import routes
