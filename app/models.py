880c93860455bf9bd98f72f39ad0a6b2cf9f947f
Ismail S. Dawjee <ismail.dawjee@gmail.com>
2025-04-13 12:44:22 +0200

    Created Post model and posts migration and table

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from typing import Optional
-
+from datetime import datetime, timezone
 import sqlalchemy as sql
 import sqlalchemy.orm as orm
 
@@ -14,5 +14,24 @@ class User(db.Model):
     email: orm.Mapped[str] = orm.mapped_column(sql.String(64), index=True, unique=True)
     password_hash: orm.Mapped[Optional[str]] = orm.mapped_column(sql.String(265))
 
+    # Virtual field
+    posts: orm.WriteOnlyMapped["Post"] = orm.relationship(back_populates="author")
+
     def __repr__(self):
-        return "<User {}:{}>".format(self.username, self.email)
+        return "<User {}: {}>".format(self.id, self.username)
+
+
+class Post(db.Model):
+    id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
+    body: orm.Mapped[str] = orm.mapped_column(sql.String(140))
+    timestamp: orm.Mapped[datetime] = orm.mapped_column(
+        index=True, default=lambda: datetime.now(timezone.utc)
+    )
+
+    # Relationships
+    user_id: orm.Mapped[int] = orm.mapped_column(sql.ForeignKey(User.id), index=True)
+    # Virtual field
+    author: orm.Mapped[User] = orm.relationship(back_populates="posts")
+
+    def __repr__(self):
+        return "<Post {}: {}>".format(self.id, self.body)
--- microblog.py
+++ microblog.py
@@ -1,1 +1,10 @@
-from app import app
+import sqlalchemy as sql
+import sqlalchemy.orm as orm
+
+from app import app, db
+from app.models import Post, User
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {"sql": sql, "orm": orm, "db": db, "User": User, "Post": Post}
